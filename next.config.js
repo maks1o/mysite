48553b4a79c5a28bf9b9ac60683ebe53ed385e89
Ivan Tkachuk <ivtkac@gmail.com>
2022-05-10 08:19:56 +0300

    Add docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    swcMinify: true,
+    outputStandalone: true,
+  },
 }
 
 module.exports = nextConfig
